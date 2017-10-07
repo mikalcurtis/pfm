ed149bc0a779e9767b55a53aa75d14ecdd554003
mikalcurtis <mikalcurtis@yahoo.com>
2017-10-07 13:54:43 -0400

    notes for later

--- work/get_ga.cmd
+++ work/get_ga.cmd
@@ -1,6 +1,7 @@
 set PATH=%PATH%;C:\devutils\GetGnuWin32\bin
 set PATH=%PATH%;C:\unxutils\bin\usr\local\wbin
-
+rem wget-1.12 -O \temp\all.html --post-data="nursing=50&returnfirst=ALL&sort=name&ORSMENU=Directory" http://167.193.144.247/facsearchs.asp  --save-cookies=\temp\mycookies.txt --keep-session-cookies  http://167.193.144.247
+rem wget-1.12 --cookies=on --keep-session-cookies --load-cookies=\temp\mycookies.txt -O \temp\ga_1.txt http://167.193.144.247/facsearchs.asp?currentpage=1
 
 for /F %%i in (sm_zips.txt) do (
 	set OUTFILE=zip_%%i.txt
